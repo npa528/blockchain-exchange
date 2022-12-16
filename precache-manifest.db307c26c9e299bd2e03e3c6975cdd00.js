4b3575ce82fae9ba29f7721eff41b02a9ee981fd
nikos papadakis <npapadakis@gmail.com>
2022-12-16 18:21:55 +0200

    Updates

---
+++ precache-manifest.db307c26c9e299bd2e03e3c6975cdd00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0f0f0d82e7c3ac79b876ca4709f0c4a",
+    "url": "/blockchain-exchange/index.html"
+  },
+  {
+    "revision": "acb9eba8c1fe2f558a07",
+    "url": "/blockchain-exchange/static/css/2.5dcc52c6.chunk.css"
+  },
+  {
+    "revision": "0619cec8cbfcbf7d2346",
+    "url": "/blockchain-exchange/static/css/main.79e9d92f.chunk.css"
+  },
+  {
+    "revision": "acb9eba8c1fe2f558a07",
+    "url": "/blockchain-exchange/static/js/2.a4204161.chunk.js"
+  },
+  {
+    "revision": "b72503c2ab0fc9bfd3e868a43bfed101",
+    "url": "/blockchain-exchange/static/js/2.a4204161.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0619cec8cbfcbf7d2346",
+    "url": "/blockchain-exchange/static/js/main.7b94d13e.chunk.js"
+  },
+  {
+    "revision": "a93aedbdd926e3c63f34",
+    "url": "/blockchain-exchange/static/js/runtime-main.e1e99a3e.js"
+  }
+]);
